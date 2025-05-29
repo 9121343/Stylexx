813370c66cc53a8b176587001beb1fddf7aadb3c
Madhur Kharade <madhurkharade9@gmail.com>
2025-05-30 01:16:01 +0530

    Fix build: remove missing ShopPage import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ShopPage from "@/components/ShopPage";
+//import ShopPage from "@/components/ShopPage";
 import { CartProvider } from "@/components/CartContext";
 import "./styles.css"; // or wherever Tailwind CSS is imported
 
