c4be82921ec5bcee144b3f9b26bf73ded11a79f4
Madhur Kharade <madhurkharade9@gmail.com>
2025-05-30 01:21:23 +0530

    Remove missing ShopPage import and usage

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 // import ShopPage from "@/components/ShopPage";
 import { CartProvider } from "@/components/CartContext";
-import "./styles.css"; // or wherever Tailwind CSS is imported
+import "./styles.css";
 
 export default function App() {
   return (
     <CartProvider>
       {/* <ShopPage /> */}
-      {/* You can put some fallback UI here */}
       <div>Welcome to Stylexx!</div>
     </CartProvider>
   );
