26ec40ab8e9fd7bf8a7cae2776a7173f38676d8e
Madhur Kharade <madhurkharade9@gmail.com>
2025-05-30 01:52:11 +0530

    Add ShopPage component

---
+++ src/components/ShopPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShopPage = () => {
+  return (
+    <div>
+      <h2>Shop Page</h2>
+      <p>This is the Shop page content.</p>
+    </div>
+  );
+};
+
+export default ShopPage;
